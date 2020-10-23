bbfbd077512235345192ab6c39084de1f11a5727
nawazsky <nawaz.ahmed@sky.uk>
2020-10-23 17:25:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 ({
     processData : function (component, event, helper){
         component.set("v.showSpinner",true);
-        const fileInput = component.find("fileRawData").getElement();
-        const file = fileInput.files[0];
     },
 })
