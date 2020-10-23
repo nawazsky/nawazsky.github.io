dc89074d3b538e86da0305d27fbc4e11f812794e
nawazsky <nawaz.ahmed@sky.uk>
2020-10-23 17:40:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,61 +1,26 @@
 ({
-    CSV2JSON : function (component,csv) {
-        var arr = [];
-        arr =  csv.split('\n');
-        var jsonObj = [];
-        var headers = arr[0].split(',');
-        for(var i = 1; i < arr.length; i++) {
-            var data = arr[i].split(',');
-            var obj = {};
-            for(var j = 0; j < data.length; j++) {
-                obj[headers[j].trim()] = data[j].trim();
-            }
-            jsonObj.push(obj);
-        }
-        return JSON.stringify(jsonObj);
-    },
-
-    insertCampaignMembersHelper : function (component,event,helper) {
+    processData : function (component, event, helper){
         component.set("v.showSpinner",true);
-        component.set('v.columns', [
-            {label: 'Description', fieldName: 'labelName', type: 'text'},
-            {label: 'Count', fieldName: 'resultCount', type: 'Integer'}
-        ]);
-        var campMemberInsert = component.get("c.insertMembersFromCSVFile");
-        campMemberInsert.setParams({
-            "dataFromSpreadsheet":component.get("v.jsonData"),
-            "campaignId":component.get("v.recordId")
-        });
-        campMemberInsert.setCallback(this, function(response){
-            var toastEvent = $A.get("e.force:showToast");
-            var state = response.getState();
-            if (state === "SUCCESS") {
-                var responseText = response.getReturnValue();
-                if(responseText.isSuccess){
-                    component.set("v.showcard",false);
-                    component.set("v.showSpinner",false);
-                    component.set('v.resultData', responseText.listResults);
-                    component.set("v.displayResultData",true);
-                }
-                if(!responseText.isSuccess){
-                    toastEvent.setParams({
-                        "type":"error",
-                        "duration":3000,
-                        "title": "ERROR",
-                        "message": responseText.errorMessage
-                    });
-                    toastEvent.fire();
-                    $A.get("e.force:closeQuickAction").fire();
-                    $A.get('e.force:refreshView').fire();
-                    component.set("v.showSpinner",false);
-                }
+        const fileInput = component.find("fileRawData").getElement();
+        const file = fileInput.files[0];
+        if (file){
+            var reader = new FileReader();
+            reader.readAsText(file, "UTF-8");
+            reader.onload = function (evt) {
+                var csv = evt.target.result;
+                var result = helper.CSV2JSON(component,csv);
+                component.set("v.jsonData",result);
+                component.set("v.showSpinner",false);
+                component.set("v.showcard",true);
             }
-        });
-        $A.enqueueAction(campMemberInsert);
+        }
+    };
+
+    insertCampaignMembers : function(component, event, helper) {
+        helper.insertCampaignMembersHelper(component,event);
     },
 
-    closeModalHelper : function(component,event,helper){
-        $A.get("e.force:closeQuickAction").fire();
-        $A.get('e.force:refreshView').fire();
-	},
+    closeModal : function(component,event,helper){
+        helper.closeModalHelper(component,event);
+	};
 })
