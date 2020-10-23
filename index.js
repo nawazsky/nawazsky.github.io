6f2ac16f409341e50a0d88606862dfe4e6239706
nawazsky <nawaz.ahmed@sky.uk>
2020-10-23 17:11:33 +0100

    Update and rename index.html to index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+({
+    processData : function (component, event, helper){
+        component.set("v.showSpinner",true);
+        const fileInput = component.find("fileRawData").getElement();
+        const file = fileInput.files[0];
+        if (file){
+            var reader = new FileReader();
+            reader.readAsText(file, "UTF-8");
+            reader.onload = function (evt) {
+                var csv = evt.target.result;
+                var result = helper.CSV2JSON(component,csv);
+                component.set("v.jsonData",result);
+                component.set("v.showSpinner",false);
+                component.set("v.showcard",true);
+            }
+        }
+    },
+
+    insertCampaignMembers : function(component, event, helper) {
+        helper.insertCampaignMembersHelper(component,event);
+    },
+
+    closeModal : function(component,event,helper){
+        helper.closeModalHelper(component,event);
+	},
+})
