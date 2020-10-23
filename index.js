6974e87c9eba0422a09a8e14ea72b06216719210
nawazsky <nawaz.ahmed@sky.uk>
2020-10-23 17:24:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,16 +3,5 @@
         component.set("v.showSpinner",true);
         const fileInput = component.find("fileRawData").getElement();
         const file = fileInput.files[0];
-        if (file){
-            var reader = new FileReader();
-            reader.readAsText(file, "UTF-8");
-            reader.onload = function (evt) {
-                var csv = evt.target.result;
-                var result = helper.CSV2JSON(component,csv);
-                component.set("v.jsonData",result);
-                component.set("v.showSpinner",false);
-                component.set("v.showcard",true);
-            }
-        }
     },
 })
